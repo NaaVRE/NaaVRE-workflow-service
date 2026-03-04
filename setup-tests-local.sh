807f547abc4431534fa1cc861c113dc277e99be6
skoulouzis <koulouzis@gmail.com>
2026-03-04 21:08:24 +0100

    do not install naavre

--- setup-tests-local.sh
+++ setup-tests-local.sh
@@ -218,15 +218,11 @@ get_auth_token(){
   fi
 
   export AUTH_TOKEN
-
-  # Test auth token with python jwt library
-  echo "----------------------------------"
   python3 -c "import jwt; import os; token = os.getenv('AUTH_TOKEN'); decoded = jwt.decode(token, options={'verify_signature': False}); print(decoded)"
   if [ $? -ne 0 ]; then
       echo "Failed to decode AUTH_TOKEN with python jwt library"
       exit 1
   fi
-
 }
 
 setup_authentication() {
